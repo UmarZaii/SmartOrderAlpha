f8096f5dab25fe6e378b4a51401f6781f3cd68d6
mansoull <mansoul96@gmail.com>
2017-02-15 03:59:11 +0800

    uvuwewe

--- app/src/main/java/com/davon/smartorderalpha/StaffSetting.java
+++ app/src/main/java/com/davon/smartorderalpha/StaffSetting.java
@@ -6,11 +6,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 /**
@@ -50,6 +54,37 @@ public class StaffSetting extends Fragment {
                 }
             }
         };
+        btnChgPasswordStaff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (view == btnChgPasswordStaff){
+                    progressDialog.setMessage("Reset Password. Please Wait...");
+                    progressDialog.show();
+
+                    String strEmail = LoginActivity.strEmail;
+
+                    if (TextUtils.isEmpty(strEmail)){
+                        Toast.makeText(getActivity(), "Enter your registered email id", Toast.LENGTH_SHORT).show();
+                        return;
+                    } else {
+                        fAuth.sendPasswordResetEmail(strEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+
+                                    progressDialog.dismiss();
+                                    Toast.makeText(getActivity(), "Your password has been reset. Please check your email!", Toast.LENGTH_SHORT).show();
+
+                                } else {
+                                    progressDialog.dismiss();
+                                    Toast.makeText(getActivity(), "Failed to send reset email!", Toast.LENGTH_LONG).show();
+                                }
+                            }
+                        });
+                    }
+                }
+            }
+        });
 
         btnLogoutStaff.setOnClickListener(new View.OnClickListener() {
             @Override
